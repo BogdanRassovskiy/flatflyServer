cbe3279e3a8d079e126250cf722651d8f7dc5860
Bogdan <131510488+BogdanRassovskiy@users.noreply.github.com>
2026-03-06 11:51:17 +0100

    mass git

--- flatflyServer/urls.py
+++ flatflyServer/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, re_path, include
 from django.views.generic import TemplateView
+from django.views.generic import RedirectView
 from django.views.static import serve
 from django.conf import settings
 from django.conf.urls.static import static
@@ -9,6 +10,7 @@ from . import views
 
 
 urlpatterns = [
+    path('admin', RedirectView.as_view(url='/admin/', permanent=True)),
     # админка (по желанию)
     path('admin/', admin.site.urls),
     # OAuth backend
